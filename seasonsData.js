3d529ec2f7048697eafe227494fbb5193bc53370
Scott <swschipke@gmail.com>
2019-11-20 21:12:42 -0700

    Clean seasonsData to include winner as a string

--- seasonsData.js
+++ seasonsData.js
@@ -1,68 +1,69 @@
 const seasonsData = [{
   season_number: 1,
-  winner_id: 12,
+  winner: 'Bebe Zahara Benet',
   image_url: 'https://vignette3.wikia.nocookie.net/logosrupaulsdragrace/images/8/81/Rpdr_season1.jpg',
   queens: [1, 2, 3, 4, 5, 9, 10, 11, 12]
 },
 {
   season_number: 2,
-  winner_id: 24,
+  winner: 'Tyra Sanchez',
   image_url: 'https://vignette1.wikia.nocookie.net/logosrupaulsdragrace/images/c/c0/Season2cast.png',
   queens: [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 },
 {
   season_number: 3,
-  winner_id: 36,
+  winner: 'Raja',
   image_url: 'https://vignette2.wikia.nocookie.net/logosrupaulsdragrace/images/e/e7/RPDRS3.jpg',
   queens: [13, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
 },
 {
   season_number: 4,
-  winner_id: 49,
+  winner: 'Sharon Needles',
   image_url: 'https://vignette3.wikia.nocookie.net/logosrupaulsdragrace/images/7/73/Rpdr_season4.jpg',
   queens: [37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49]
 },
 {
   season_number: 5,
-  winner_id: 63,
+  winner: 'Jinkx Monsoon',
   image_url: 'https://vignette2.wikia.nocookie.net/logosrupaulsdragrace/images/b/b6/S5cast.jpg',
   queens: [50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63]
 },
 {
   season_number: 6,
-  winner_id: 80,
+  winner: 'Bianca Del Rio',
   image_url: 'https://vignette3.wikia.nocookie.net/logosrupaulsdragrace/images/d/d7/S6cast.jpg',
   queens: [64, 65, 66, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80]
 },
 {
   season_number: 7,
-  winner_id: 94,
+  winner: 'Violet Chachki',
   image_url: 'https://vignette2.wikia.nocookie.net/logosrupaulsdragrace/images/d/d8/S7cast.jpg',
   queens: [81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94]
 },
 {
   season_number: 8,
-  winner_id: 106,
+  winner: 'Bob the Drag Queen',
   image_url: 'https://vignette3.wikia.nocookie.net/logosrupaulsdragrace/images/c/c1/S8cast.jpg',
   queens: [96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 95]
 },
 {
   season_number: 9,
-  winner_id: 124,
+  winner: 'Sasha Velour',
   image_url: 'https://vignette4.wikia.nocookie.net/logosrupaulsdragrace/images/d/d5/97FMqTL.jpg',
   queens: [97, 107, 108, 109, 110, 111, 112, 113, 114, 118, 125, 119, 123, 124]
 },
 {
   season_number: 10,
-  winner_id: 126,
+  winner: 'Aquaria',
   image_url: 'https://vignette.wikia.nocookie.net/logosrupaulsdragrace/images/a/af/RPDR_S10_Banner.jpeg',
   queens: [126, 131, 134, 127, 132, 129, 138, 135, 136, 137, 130, 133, 110, 128]
 },
 {
   season_number: 11,
-  winner_id: 153,
+  winner: 'Yvie oddly',
   image_url: 'https://vignette.wikia.nocookie.net/logosrupaulsdragrace/images/8/83/S11Banner.jpg',
   queens: [152, 151, 150, 149, 137, 148, 147, 146, 145, 144, 143, 142, 141, 140, 139]
 }]
+
 
 module.exports = seasonsData;
