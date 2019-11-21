7e9f1a506a5e040cd9677ad95bce55a6f4f47ed0
Scott <swschipke@gmail.com>
2019-11-21 08:59:00 -0700

    Create initial database migration

---
+++ db/migrations/20191120212915_initial.js
@@ -0,0 +1,32 @@
+
+exports.up = function (knex) {
+  return Promise.all([
+    knex.schema.createTable("seasons", function (table) {
+      table.increments("id").primary();
+      table.integer("number");
+      table.string("winner");
+      table.string("image_url");
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable("queens", function (table) {
+      table.increments("id").primary();
+      table.string("name");
+      table.integer('queen_id')
+      table.boolean("winner");
+      table.boolean("missCongeniality");
+      table.string("qote");
+      table.integer("season").unsigned();
+      table.integer("season_id").unsigned();
+      table.foreign("season_id").references("seasons.id");
+      table.timestamps(true, true);
+    })
+  ]);
+};
+
+exports.down = function (knex) {
+  return Promise.all([
+    knex.schema.dropTable("queens"),
+    knex.schema.dropTable("seasons")
+  ]);
+};
